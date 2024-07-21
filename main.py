4e380fc2be19b48ba5624c656f8fdc57cdcc6bcb
SandeepGarikapati <98882254+SandeepGarikapati@users.noreply.github.com>
2024-07-21 20:44:02 +0530

    Created procfile

--- main.py
+++ main.py
@@ -63,7 +63,7 @@ class Base(DeclarativeBase):
     pass
 
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DB_URI", "sqlite:///posts.db")
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
 
@@ -306,4 +306,4 @@ def contact():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5001)
+    app.run(debug=False)
